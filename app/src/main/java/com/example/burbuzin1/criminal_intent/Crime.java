177bad5460ba98fc78425134a49dc989800b9bf9
burbuzin1 <tolkachev.dima@gmail.com>
2015-06-22 21:42:42 +0300

    8. Макеты и виджеты

--- app/src/main/java/com/example/burbuzin1/criminal_intent/Crime.java
+++ app/src/main/java/com/example/burbuzin1/criminal_intent/Crime.java
@@ -1,13 +1,34 @@
 package com.example.burbuzin1.criminal_intent;
 
+import java.util.Date;
 import java.util.UUID;
 
 public class Crime {
+    private Date mDate;
+    private boolean mSolved;
 
     private UUID mId;
     private String mTitle;
+
     public Crime() {
         mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+      mSolved = solved;
     }
 
     public UUID getId() {
